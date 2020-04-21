a7e0d9b05438a7bfc5769a4a969404bcf4e1bb38
Julio Dutra <jjcdutra@gmail.com>
2020-04-21 10:36:38 -0300

    Criando novos projetos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -8,10 +8,20 @@ export default function App() {
 
   useEffect(() => {
     api.get('projects').then(response => {
-      console.log(response.data);
       setProjects(response.data);
     });
   }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Julio Dutra'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
 
   return (
     <>
@@ -25,6 +35,14 @@ export default function App() {
             <Text style={styles.project}>{project.title}</Text>
           )}
         />
+
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+            <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -39,5 +57,19 @@ const styles = StyleSheet.create({
   project: {
     color: '#FFF',
     fontSize: 29,
+  },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16
   }
 });
